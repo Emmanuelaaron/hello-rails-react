d41669cb2e3cdbde4bb3781a7bf26372ce1697e4
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-12-03 21:34:07 +0300

    sets up styleint

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,9 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Greeting from "./Greeting"
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../components/style.css'
 import Store from "../redux/ConfigureStore";
 
 class App extends React.Component {
